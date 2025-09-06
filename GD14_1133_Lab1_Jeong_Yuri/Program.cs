467a53f2b085057732ce2bb574bedd1f4e4d559b
Epicjeong <yurijeong@icloud.com>
2025-09-05 20:53:17 -0700

    Mess around in visual studio

---
+++ GD14_1133_Lab1_Jeong_Yuri/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD14_1133_Lab1_Jeong_Yuri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, World!");
+            Console.WriteLine("Whatever youwant!");
+        }
+    }
+}
