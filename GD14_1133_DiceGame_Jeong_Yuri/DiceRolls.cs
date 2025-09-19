5a7e79bd90b3676ce2341398da603a2822071b29
Epicjeong <yurijeong@icloud.com>
2025-09-18 17:18:56 -0700

    The start of the dice game
    It shall be improved on lab 4 I think

---
+++ GD14_1133_DiceGame_Jeong_Yuri/DiceRolls.cs
@@ -0,0 +1,42 @@
+﻿using GD14_1133_DiceGame_Jeong_Yuri.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri
+{
+    internal class DiceRolls
+    {
+        //Stores the total value of all rolls
+        int TotalRoll;
+
+        //Rolls various dice once by creating an instance of Random, starting from d6 and increasing to d8, d12, and d20
+        internal void RollDice(int MinRoll = 1, int MaxRoll = 6)
+        {
+            //The starting d6 roll
+            Random random = new Random();
+            int Roll1 = random.Next(MinRoll, MaxRoll);
+            Console.WriteLine("The d6 rolled a " + Roll1);
+
+            //Increases to a d8 and rolls again
+            MaxRoll = 8;
+            int Roll2 = random.Next(MinRoll, MaxRoll);
+            Console.WriteLine("The d8 rolled a " + Roll2);
+
+            //Increases to a d12 and rolls again
+            MaxRoll = 12;
+            int Roll3 = random.Next(MinRoll, MaxRoll);
+            Console.WriteLine("The d12 rolled a " + Roll3);
+
+            //Increases to a d20 and rolls again
+            MaxRoll = 20;
+            int Roll4 = random.Next(MinRoll, MaxRoll);
+            Console.WriteLine("The d20 rolled a " + Roll4);
+
+            TotalRoll = Roll1 + Roll2 + Roll3 + Roll4;
+            Console.WriteLine(TotalRoll);
+        }
+    }
+}
---
+++ GD14_1133_DiceGame_Jeong_Yuri/Program.cs
@@ -0,0 +1,15 @@
+﻿using GD14_1133_DiceGame_Jeong_Yuri.Scripts;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri
+{
+    internal class Program
+    {
+        
+        //Instanciates the game controller
+        static void Main(string[] args)
+        {
+            GameController controller = new GameController();
+            controller.PlayGame();
+        }
+    }
+}
