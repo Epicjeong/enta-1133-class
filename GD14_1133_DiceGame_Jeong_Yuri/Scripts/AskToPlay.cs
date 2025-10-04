8d9e90b061a2bfd7a0c3c22ee65b4e424e813248
Epicjeong <yurijeong@icloud.com>
2025-10-03 18:41:21 -0700

    The entire assignment 1
    Submit the assignment to github

---
+++ GD14_1133_DiceGame_Jeong_Yuri/Scripts/AskToPlay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri.Scripts
+{
+    /// <summary>
+    /// Asks to play the game
+    /// Yeah thats about it
+    /// </summary>
+    internal class AskToPlay
+    {
+        internal void PlayMyGame(Player player, Computer computer, DiceRolls diceroller, ScoreKeeper scoreKeep, GameController gameController, EndGame endGame)
+        {
+            //Instantiates the player
+            string answer = Console.ReadLine();
+            //If yes, explains the rules
+            if (answer == "y")
+            {
+                Console.WriteLine("Excellent, now we explain the rules");
+                Console.WriteLine("");
+                Console.WriteLine("You and your computer opponent will start with a number sides, each turn you will decide how many sides you wish to use");
+                Console.WriteLine("The amount of sides you use is how many sides the dice you will be rolling has, so with 7 sides your highest score is 7");
+                Console.WriteLine("After you both roll, whoever has the highest score wil be awarded a point. Whoever gets more points in 4 rounds wins");
+                Console.WriteLine("");
+                Console.WriteLine("If both die roll the same result then no points will be awarded and your sides will not be refunded");
+                Console.WriteLine("If both amount of points by the end are the same, the winner will be whoever has the lowest total rolls");
+                Console.WriteLine("If both total rolls are the same, the remaining sides will be used for a final roll to decide thw winner");
+                Console.WriteLine("If you have 0 points at the start of a new round, you will automatically lose that round");
+                //The player goes first so they get the instructions before the computer does anything
+                Console.WriteLine("");
+                Console.WriteLine("As you are the person entering the game, you will choose how much sides you will roll with first");
+                Console.WriteLine("But first, choose how many points to start with (minimum starting sides are 10, the computer will always start with 50)");
+                //Sets the amount of sides the player starts with
+                player.SetPlayerSides();
+                //Begins the players first turn
+                //Because the scorekeeper loops using PlayerTurn, using PlayerTurn here would add additional rounds of the game, hence why a different function is called
+                player.PlayerTurn(player, computer, diceroller, scoreKeep, gameController, endGame);
+            }
+            //Can't exactly do much if you don't want to play
+            else if (answer == "n")
+            {
+                Console.WriteLine("ok");
+            }
+            //If you didn't input one of the 2 options
+            else
+            {
+                Console.WriteLine("Please enter a valid option");
+                PlayMyGame(player, computer, diceroller, scoreKeep, gameController, endGame);
+            }
+        }
+    }
+}
---
+++ GD14_1133_DiceGame_Jeong_Yuri/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using GD14_1133_DiceGame_Jeong_Yuri.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD14_1133_DiceGame_Jeong_Yuri
+{
+    /// <summary>
+    /// The players class
+    /// Proceeds through the players turn
+    /// Stores some player specific values
+    /// </summary>
+    internal class Player
+    {
+        //Keeps track of the player's dice rolls, the amount of sides they have left, and the total of all dice rolls
+        private int playerRoll;
+        private int playerScore;
+        private int playerSidesLeft;
+        int playerDiceResult;
+
+        internal void SetPlayerSides()
+        {
+            //Sets the amount of sides the player starts with
+            int.TryParse(Console.ReadLine(), out int startingSides);
+            if (startingSides >= 10)
+            {
+                playerSidesLeft = startingSides;
+            }
+            //The reason the minimum is 10 is because I feel any below that would be unfun, at 10 there is at least some sembalence of chance
+            else
+            {
+                Console.WriteLine("Please input a valid number");
+                SetPlayerSides();
+            }
+        }
+
+        //The players turn
+        internal void PlayerTurn(Player player, Computer computer, DiceRolls diceroller, ScoreKeeper scoreKeep, GameController gameController, EndGame endGame)
+        {
+            //Creates the instance of DiceRolls so the player can choose and roll their dice
+            Console.WriteLine("You have " + playerSidesLeft + " more sides left");
+            if ( playerSidesLeft == 0)
+            {
+                Console.WriteLine("As you have 0 sides left your turn will be forfiet");
+                playerDiceResult = 0;
+            }
+            else
+            {
+                diceroller.RollDice(player);
+                playerDiceResult = diceroller.GetDiceResult();
+            }
+
+            //Adds the dice result to the players total points and removes the sides they declared
+            playerRoll = playerDiceResult;
+            playerScore = playerScore + playerDiceResult;
+            int sidesUsed = diceroller.GetSidesUsed();
+            playerSidesLeft = playerSidesLeft - sidesUsed;
+            Console.WriteLine("Your total score is now " + playerScore);
+            //Puts breaks every now and again so the player has time to read
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+
+            //Continues to either the computers turn or to compare the rolls if thats already happened
+            if (diceroller.GetComputerHasRolled() == false)
+            {
+                computer.ComputerTurn(player, computer, diceroller, scoreKeep, gameController, endGame);
+            }
+            else
+            {
+                scoreKeep.KeepScore(player, computer, diceroller, scoreKeep, gameController, endGame);
+            }
+        }
+
+        //Lets other classes get the players roll
+        internal int GetPlayerRoll()
+        {
+            return playerRoll;
+        }
+        //Lets other classes get the amount of sides the player has left
+        internal int GetPlayerSidesLeft()
+        {
+            return playerSidesLeft;
+        }
+        //Lets other classes get the players score
+        internal int GetPlayerScore()
+        {
+            return playerScore;
+        }
+    }
+}
